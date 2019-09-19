9a5415084eb6b8bd860a7f5069e6e302df4690e6
simochiaro <55441405+simochiaro@users.noreply.github.com>
2019-09-19 09:30:56 +0200

    Add files via upload

--- Matrix.h
+++ Matrix.h
@@ -34,7 +34,7 @@ public:
     };
 
 
-    void setValue(T t,int m,int n){
+    const void setValue(T t,int m,int n){
 
         if ((m<0 || m >= numRow) || (n<0 || n >= numCol)) {
             throw std::invalid_argument("Out of index");  //Esempio di gestione di eccezioni nel main
@@ -53,7 +53,7 @@ private:
         }
     }
 public:
-    T getValue(int m,int n){
+    const T getValue(int m,int n){
         if((m<=0 || m >= numRow) || (n<0 || n >= numCol)){
             throw std::invalid_argument("Out of index");
 
